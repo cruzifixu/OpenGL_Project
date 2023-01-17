66612d60ab2f3a93417bb0d9ba283873cf37e715
cruzifixu <franchezcacruz09@gmail.com>
2023-01-17 20:50:45 +0100

    set up done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,67 @@
 #include <iostream>
-#define GLEW_STATIC
-#include <GL/glew.h>
+//#define GLEW_STATIC
+#include <gl/glew.h>
 #include <GLFW/glfw3.h>
 
 const GLint WIDTH = 800, HEIGHT = 600;
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	// core = new OPENGL stuff
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL Project", nullptr, nullptr);
+	
+	// acutal width of window itself like identity
+	int screenWidth, screenHeight;
+	glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+
+	if (nullptr == window)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+
+		return EXIT_FAILURE;
+	}
+	
+	glfwMakeContextCurrent(window);
+
+	// for modern approach not experimental
+	glewExperimental = GL_TRUE;
+
+	if (GLEW_OK != glewInit())
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		
+		return EXIT_FAILURE;
+	}
+
+	// get actual representation of window screen
+	glViewport(0, 0, screenWidth, screenHeight);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		// if not put its gon be blank
+		// 1 intensity of red green blue
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// draw opengl stuff
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+
+	return EXIT_SUCCESS;
+}
 
 
